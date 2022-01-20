198213e453717285fe6fc11b1c8139dfbdac80c3
Ariful Islam <95134736+Arifulislam999@users.noreply.github.com>
2022-01-20 21:32:00 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
-let btn = document.querySelector(".men");
-let menu = document.querySelector(".logo-right");
-let cont = document.querySelector("#con");
-
-btn.addEventListener("click", () => {
-    btn.classList.toggle("togle");
-
-    if (btn.classList.contains("togle")) {
-        menu.style.display = "inline";
-        menu.style.transition = "all .4s";
-        cont.style.transform = "translateY(260px)";
-        cont.style.transition = "all .3s";
-    } else {
-        menu.style.transition = "all .4s";
-        menu.style.display = "none";
-        cont.style.transform = "translateY(0px)";
-        cont.style.transition = "all .3s";
-    }
+let x = document.querySelector(".alrt");
+let bt = document.querySelector(".bt");
+bt.addEventListener("click", () => {
+    bt.style.display = "none";
+    x.style.display = "inline";
 });
+x.style.display = "none";
