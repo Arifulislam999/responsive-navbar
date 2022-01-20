4cbfda078e7fdae161702c0d3eaf523b4c7251ba
Ariful Islam <95134736+Arifulislam999@users.noreply.github.com>
2022-01-21 01:30:47 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let x = document.querySelector(".alrt");
-let bt = document.querySelector(".bt");
-bt.addEventListener("click", () => {
-    bt.style.display = "none";
-    x.style.display = "inline";
-});
-x.style.display = "none";
+// let x = document.querySelector(".alrt");
+// let bt = document.querySelector(".bt");
+// bt.addEventListener("click", () => {
+//     bt.style.display = "none";
+//     x.style.display = "inline";
+// });
+// x.style.display = "none";
+// let arif = document.querySelector("#arif");
